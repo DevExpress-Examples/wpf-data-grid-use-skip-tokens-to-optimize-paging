ced83a5f82ae00fde7d3de66b8e7c7b019309453
DevExpressExampleBot <fakeEmail>
2023-12-07 14:21:24 +0400

    Source auto update [skip ci]

--- VB/IssueService/IssuesFetchResult.vb
+++ VB/IssueService/IssuesFetchResult.vb
@@ -1,27 +1,32 @@
-﻿Namespace PagedAsyncSourceSkipTokenSample
-	Public Class IssuesFetchResult
-		Public Sub New(ByVal issues() As IssueData, ByVal nextSkipToken As Object)
-			Me.Issues = issues
-			Me.NextSkipToken = nextSkipToken
-		End Sub
+Namespace PagedAsyncSourceSkipTokenSample
 
-		Private privateIssues As IssueData()
-		Public Property Issues() As IssueData()
-			Get
-				Return privateIssues
-			End Get
-			Private Set(ByVal value As IssueData())
-				privateIssues = value
-			End Set
-		End Property
-		Private privateNextSkipToken As Object
-		Public Property NextSkipToken() As Object
-			Get
-				Return privateNextSkipToken
-			End Get
-			Private Set(ByVal value As Object)
-				privateNextSkipToken = value
-			End Set
-		End Property
-	End Class
+    Public Class IssuesFetchResult
+
+        Private _Issues As PagedAsyncSourceSkipTokenSample.IssueData(), _NextSkipToken As Object
+
+        Public Sub New(ByVal issues As IssueData(), ByVal nextSkipToken As Object)
+            Me.Issues = issues
+            Me.NextSkipToken = nextSkipToken
+        End Sub
+
+        Public Property Issues As IssueData()
+            Get
+                Return _Issues
+            End Get
+
+            Private Set(ByVal value As IssueData())
+                _Issues = value
+            End Set
+        End Property
+
+        Public Property NextSkipToken As Object
+            Get
+                Return _NextSkipToken
+            End Get
+
+            Private Set(ByVal value As Object)
+                _NextSkipToken = value
+            End Set
+        End Property
+    End Class
 End Namespace
